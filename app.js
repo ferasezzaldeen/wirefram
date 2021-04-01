d948d2b76e65db7492b82d2a44480316ca37434d
FerasSadTawfiqEzz-aldeen <Ferasezaldeen@gmail.com>
2021-04-01 13:38:37 +0300

    pushing for lab04

---
+++ app.js
@@ -0,0 +1,16 @@
+alert('welcome to my page');
+var nAme = prompt("please enter your name", "your name");
+alert('thank you for visiting our page ' + nAme);
+var dSg = prompt("are you a barcelona fan " + nAme + " ??", "yes or no");
+console.log(dSg);
+if (dSg === "yes")
+{
+    alert("welcome abored cules to this fantastic page !!!!!!");
+}
+else
+{
+    alert('you are missing alot of fun');
+}
+
+
+

5e5611306920effedad1eedd8a662371d2129c3f
FerasSadTawfiqEzz-aldeen <Ferasezaldeen@gmail.com>
2021-04-04 14:40:49 +0300

    pushing for lab05

--- app.js
+++ app.js
@@ -12,5 +12,25 @@ else
     alert('you are missing alot of fun');
 }
 
+var player = prompt("who is your favorite player, messi or dempele??");
+while(player !== 'messi' && player !== 'dempele'){
+    player = prompt("who is your favorite player, messi or dempele??");
+};
+var n = prompt("how many time you want his picture to be present to you ??");
+function NumberOfPictures(player,n) {
+if(player == "dempele"){
+    for(var i=0;i<n;i++){
+        document.write('<img src="https://cdn.vox-cdn.com/thumbor/rv7ZSsczjXYR3uLGq6i4JE2lS_A=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22303478/1231141173.jpg" />')
+    }
+
+}
+else{for(var i=0;i<n;i++){
+    document.write('<img src="https://cdn-japantimes.com/wp-content/uploads/2021/03/np_file_76348.jpeg" />')
+
+}
+}
+}
+NumberOfPictures(player,n);
+
 
 

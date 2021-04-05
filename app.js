b7581e4bcf7144e8f764822f63277a1bb9181949
FerasSadTawfiqEzz-aldeen <Ferasezaldeen@gmail.com>
2021-04-05 14:42:04 +0300

    pushing for lab06

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-// alert('welcome to my page');
-// var nAme = prompt("please enter your Name", "your name");
-// alert('thank you for visiting our new page ' + nAme);
-// var dSg = prompt("are you a barcelona fan " + nAme + " ??", "yes or no");
-// console.log(dSg);
-// if (dSg === "yes")
-// {
-//     alert("welcome abored cules to this fantastic page !!!!!!");
-// }
-// else
-// {
-//     alert('you are missing alot of fun');
-// }
+alert('welcome to my page');
+var nAme = prompt("please enter your Name", "your name");
+alert('thank you for visiting our new page ' + nAme);
+var dSg = prompt("are you a barcelona fan " + nAme + " ??", "yes or no");
+console.log(dSg);
+if (dSg === "yes")
+{
+    alert("welcome abored cules to this fantastic page !!!!!!");
+}
+else
+{
+    alert('you are missing alot of fun');
+}
 
-// var player = prompt("who is your favorite player, messi or dempele??");
-// while(player !== 'messi' && player !== 'dempele'){
-//     player = prompt("who is your favorite player, messi or dempele??");
-// };
-// var n = prompt("how many time you want his picture to be present to you ??");
-// function NumberOfPictures(player,n) {
-// if(player == "dempele"){
-//     for(var i=0;i<n;i++){
-//         document.write('<img src="https://cdn.vox-cdn.com/thumbor/rv7ZSsczjXYR3uLGq6i4JE2lS_A=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22303478/1231141173.jpg" />')
-//     }
+var player = prompt("who is your favorite player, messi or dempele??");
+while(player !== 'messi' && player !== 'dempele'){
+    player = prompt("who is your favorite player, messi or dempele??");
+};
+var n = prompt("how many time you want his picture to be present to you ??");
+function NumberOfPictures(player,n) {
+if(player == "dempele"){
+    for(var i=0;i<n;i++){
+        document.write('<img src="https://cdn.vox-cdn.com/thumbor/rv7ZSsczjXYR3uLGq6i4JE2lS_A=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/22303478/1231141173.jpg" />')
+    }
 
-// }
-// else{for(var i=0;i<n;i++){
-//     document.write('<img src="https://cdn-japantimes.com/wp-content/uploads/2021/03/np_file_76348.jpeg" />')
+}
+else{for(var i=0;i<n;i++){
+    document.write('<img src="https://cdn-japantimes.com/wp-content/uploads/2021/03/np_file_76348.jpeg" />')
 
-// }
-// }
-// }
-// NumberOfPictures(player,n);
+}
+}
+}
+NumberOfPictures(player,n);
 
 
 
